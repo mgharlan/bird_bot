6bbb2b5cf88d89f059e0851871a3466a514d8f93
mgharlan <harlangmason@gmail.com>
2020-07-16 09:52:31 -0700

    added new bird commands

--- src/bot.py
+++ src/bot.py
@@ -1,4 +1,7 @@
+import argparse
+import logging
 import os
+import sys
 
 import discord
 from discord.ext import commands
@@ -7,7 +10,7 @@ class BirdBot:
   def __init__(self, token):
     self.token = token
     self.bot = commands.Bot(command_prefix='-')
-    self.bot.load_extension("birds")
+    self.bot.load_extension("botcommands")
     self.on_ready = self.bot.event(self.on_ready)
 
   def run(self):
@@ -15,8 +18,35 @@ class BirdBot:
 
 
   async def on_ready(self):
-    print(f'{self.bot.user} has connected to Discord!')
+    logging.info(f'{self.bot.user} has connected to Discord!')
+ 
+def prep_log(debug, console):
+  log_format = '%(asctime)s %(funcName)s()_%(lineno)s %(levelname)s: %(message)s'
+  date_format = '%m/%d/%Y %I:%M:%S %p'
+
+  logger = logging.getLogger()
+  logger.setLevel(logging.DEBUG)
+  formatter = logging.Formatter(log_format, datefmt=date_format)
+  if console:
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG if debug else logging.INFO)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+  file_handler = logging.FileHandler('logs/log.log')
+  file_handler.setLevel(logging.INFO)
+  file_handler.setFormatter(formatter)
+  logger.addHandler(file_handler)
+  
+def parser():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-d", "--debug", help="DEBUG MODE", action="store_true")
+  parser.add_argument("-c", "--console", help="LOG TO CONSOLE", action="store_true")
+  argv = parser.parse_args()
+
+  prep_log(argv.debug, argv.console)
  
 if __name__ == "__main__":
+  parser()
   bird = BirdBot(os.environ.get('BIRD_BOT_TOKEN'))
   bird.run()
---
+++ src/botcommands.py
@@ -0,0 +1,115 @@
+from bs4 import BeautifulSoup
+import logging
+import os
+import pandas as pd
+from pathlib import Path
+import random
+import requests
+
+import discord
+from discord.ext import commands
+
+class BotCommands(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+    
+    self.data = pd.read_csv("bird_data/bird_urls.csv")
+      
+  @commands.command(name='r', aliases=['random'], brief='for random bird facts')
+  async def send_random(self, ctx):
+    logging.info(f'{ctx.author.name} selected random')
+    
+    await ctx.send('gathering random bird...')
+    
+    number = random.randint(0,self.data.shape[0])
+    
+    URL = self.data['BIRD URLs'][number]
+    
+    await self.send_bird(ctx, URL)
+  
+  @commands.command(name='l', aliases=['list'], brief='list birds that start with the letter provided')
+  async def list(self, ctx, *args):
+    if (len(args) == 0):
+      await ctx.send('please provide a first letter to list by')
+    elif (len(args) == 1 and args[0].isalpha()):
+      bird_string = f'birds that begin with {args[0]}:'
+      searching = True
+      for URL in self.data['BIRD URLs']:
+        name = Path(URL).stem
+        if name[0] == args[0]:
+          if len(bird_string) > 1500:
+            await ctx.send(bird_string)
+            bird_string = '\t'
+          else:
+            bird_string = bird_string + f'\n\t{name}'
+          searching = False
+        elif not searching:
+          break
+      await ctx.send(bird_string)
+    else:
+      await ctx.send('please provide one first letter to list by')
+  
+  @commands.command(name='show', brief='show the bird specified')
+  async def show(self, ctx, *args):
+    if (len(args) == 0):
+      await ctx.send('please provide a bird to show')
+    elif (len(args) == 1):
+      name = args[0].lower()
+      sent = False
+      for URL in self.data['BIRD URLs']:
+        if Path(URL).stem == name:
+          await ctx.send('gathering bird...')
+          await self.send_bird(ctx, URL)
+          sent = True
+          break
+      if not sent:
+        await ctx.send(f'bird {name} was not found in my database')
+    else:
+      await ctx.send('please provide one bird to show')
+  
+  async def send_bird(self, ctx, URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    try:
+      result = soup.find('div',class_="bird-guide-image")
+      img_url = result.find('img')['src']
+      img = requests.get(img_url)
+      file = open('bird_data/bird.jpg', 'wb')
+      file.write(img.content)
+      file.close()
+      
+      image = discord.File(open(f'bird_data/bird.jpg', 'rb'))
+      os.remove("bird_data/bird.jpg")
+      await ctx.send(file=image)
+      
+    except Exception as e:
+      logging.error(e, exc_info=True)
+      logging.info(URL + ', problem with bird image ,' + number)
+      await ctx.send(f'**image for {bird_name} not found**')
+      if (os.path.exists("bird_data/bird.jpg")):
+        os.remove("bird_data/bird.jpg")
+
+    try:
+      result = soup.find('div', class_="hide-for-tiny hide-for-small hide-for-medium")
+      text = result.text.strip('\t \n')
+      await ctx.send(text)
+      
+    except Exception as e:
+      logging.error(e, exc_info=True)
+      logging.info(URL + ', problem with bird text ,' + number)
+      await ctx.send(f'**text for {bird_name} not found**')
+      
+    await ctx.send(f'*From: {URL}*')
+  
+  @commands.command(name='s', aliases=['shutdown'], hidden=True)
+  async def shut_down(self, ctx):
+    if(ctx.author.name == 'mazonly' or ctx.author.name == 'ruby 🌻'):
+      logging.info('shut down')
+      await ctx.send('Shutting Down')
+      await self.bot.logout()
+    else:
+      await ctx.send('no!')
+
+def setup(bot):
+  bot.add_cog(BotCommands(bot))
